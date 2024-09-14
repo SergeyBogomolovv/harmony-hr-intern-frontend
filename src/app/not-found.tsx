8d63c7f4fa2cefdd51e8f7e1b850138ca2e56edb
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-09-14 18:24:30 +0300

    feat: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <h1 className="font-bold text-3xl">404 | Not found</h1>
+    </div>
+  );
+}
